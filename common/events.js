6feb2a630bf85da6f34a81ecf4dad5f958b8bba2
Hamid Raza <hamidabdul1994@hotmail.com>
2017-01-07 19:33:10 +0530

    Send mail

--- common/events.js
+++ common/events.js
@@ -184,16 +184,17 @@ custEvent.prototype.readFalloutEmployee = function (date,monthDays) {
 
 custEvent.prototype.readLeaveEmployee = function (date,monthDays) {
   return new Promise(function(resolve, reject) {
-      redisClient.hgetall("employeeUnmarkedAttendance", function(error, employeeAttendance) {
+    console.log(date,monthDays);
+      redisClient.hgetall("employeeLeave", function(error, employeeLeave) {
         var obj = {};
         for (var key=1;key<=monthDays;key++) {
-        var temp =employeeAttendance[date+"/"+key]!==undefined ? (JSON.parse(employeeAttendance[date+"/"+key])): [];
+        var temp =employeeLeave[date+"/"+key]!==undefined ? (JSON.parse(employeeLeave[date+"/"+key])): [];
         var p = temp.forEach(function(value){
           if(obj[value]!==undefined)
           {
             obj[value]={"value":++obj[value].value};
           }else {
-            obj[value]={"value":0};
+            obj[value]={"value":1};
           }
         });
 
@@ -201,7 +202,7 @@ custEvent.prototype.readLeaveEmployee = function (date,monthDays) {
         Promise.all([p]).then(function(){
           var temp =[];
           for (var i in obj) {
-            if(obj[i].value>=3){
+            if(obj[i].value>=2){
               temp.push(i);
             }
           }
