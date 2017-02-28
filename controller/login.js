b60fa01fa1694e556bf3c2f078c3efe9ea8fdf76
Hamid Raza <hamidabdul1994@hotmail.com>
2017-02-28 12:28:43 +0530

    Merge with swagger API Doc

---
+++ controller/login.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var firebase = require("../config/firebase.js");
+var commonMethod = require("../common/commonMethod");
+
+router.post("/",function(req,res){
+  try {
+	console.log(req.body);
+    var email=req.body.emailId,password=req.body.password;
+    firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error) {
+      res.status(401).send({token:null,status:401,message:"Unautherized User"});
+    }).then(function(data){
+      if (data!==undefined) {
+          token=commonMethod.generateToken(email);
+          res.setHeader("x-token",token);
+      res.send({token,status:200,message:"login success"});
+      }
+    });
+  } catch(e) {
+    res.status(304).send("Bad Parameter");
+  }
+});
+
+module.exports=router;
