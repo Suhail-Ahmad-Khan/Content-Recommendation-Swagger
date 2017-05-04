390d33dac7e35f978bef900a71da0dde50ddab77
khansuhl <khansuhl@gmail.com>
2017-05-04 19:31:27 +0530

    spring-security examples

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ var path = require('path');
 
 var swagger = require("swagger-node-express");
 var argv = require('minimist')(process.argv.slice(2));
-var fs = require("fs");
 var app = express();
 var subpath = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.IP || 'localhost');
 
-app.use("/api", subpath);
 app.use(express.static('dist'));
 swagger.setAppHandler(subpath);
 swagger.setApiInfo({
@@ -23,11 +21,6 @@ swagger.setApiInfo({
     license: "",
     licenseUrl: ""
 });
-subpath.get('/', function (req, res) {
-    res.sendfile(__dirname + '/dist/index.html');
-});
-
-swagger.configureSwaggerPaths('', 'api-docs', '');
 
 var domain = 'localhost';
 if(argv.domain !== undefined)
@@ -38,8 +31,6 @@ else
 // var applicationUrl = 'http://' + domain + ':' + app.get('port');
 var applicationUrl = 'http://' + domain;
 swagger.configure(applicationUrl, '1.0.0');
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
